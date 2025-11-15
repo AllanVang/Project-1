44d13dbb1baba454844d5a2e38ef83a2f94c51a8
AllanVang <Allankpvang@yahoo.com>
2025-11-14 21:43:44 -0900

    Create Character.cpp

---
+++ Character.cpp
@@ -0,0 +1,36 @@
+#include "Character.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+Character::Character() {} //default constructor
+
+//Setting private class objects same as getters
+Character::Character(string charName, string charGender, string charSpecies)
+	: Name(charName), Gender(charGender), Species(charSpecies){}
+
+//Setter function
+void Character::setCharacter(string &charName, string &charGender, string &charSpecies)
+{
+	Name = charName;
+	Gender = charGender;
+	Species = charSpecies;
+}
+
+
+string Character::getName()
+{
+	return Name;
+}
+
+string Character::getGender()
+{
+	return Gender;
+}
+
+string Character::getSpecies()
+{
+	return Species;
+}
