d3740491b61d98c4d6a8c0e8bb436290b2d475c0
AllanVang <Allankpvang@yahoo.com>
2025-11-30 17:19:42 -0900

    Update MenuOptions.cpp

--- MenuOptions.cpp
+++ MenuOptions.cpp
@@ -15,16 +15,22 @@ void displayMenu()
 		 << "\n LeeVentures \n"
 		 << "------------\n\n"
 		 << "1. Start Game\n"
-		 << "2. Settings\n"
-		 << "3. Credits\n";
+		 << "2. Extras\n"
+		 << "3. Credits\n"
+		 << "4. Quit Game\n";
 
 }
 
 
 
-void gameSettings()
+void gameSettings() //Extras
 {
-	cout << "\nThis is the menu settings... There's not much here... yet.\n";
+	cout << "\nThis is the extras section.\n"
+		 << "This program runs order of n as it has to read from a script.txt\n"
+		 << "It could run faster if the script was hard coded into the program "
+		 << "but we didn't think that would've been very appropiate.\n"
+		 << "After all this program doesn't handle any large computations\n\n";
+
 }
 
 void gameCredits()
